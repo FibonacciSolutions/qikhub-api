7eee729c50fefe6b0d30d385960dbff16ceb61f9
Your Name <your-email@example.com>
2026-04-13 13:41:29 +0545

    Enable Swagger in production

--- Program.cs
+++ Program.cs
@@ -53,18 +53,14 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-
 // Create database on startup
 using (var scope = app.Services.CreateScope())
 {
